af032970df5111becf7f9df8b761e6766af4e6d9
Dmitry Shneider <dmitryshneider@gmail.com>
2024-04-22 18:38:46 +0600

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+
+  const PORT = process.env.PORT || 3000;
+  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(PORT);
+}
+bootstrap();
